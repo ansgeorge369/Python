9532c674253087d27b5ba28342c23d540622c331
ansgeorge369 <ansgeorge123@gmail.com>
2024-10-13 22:44:37 +0530

    variables

---
+++ variables/variables.py
@@ -0,0 +1,18 @@
+a = 5
+print(a)
+
+name = 'youtube'
+print((name + ' rocks'))
+
+print(name[0])
+print(name[-1])
+print(name[0:2])
+print(name[1:4])
+print(name[1:])
+print(name[:4])
+
+# strings in python is immutable ie once the values are assigned name[0] = u cannot be done
+
+print('my ' + name[3:])
+myname = 'Ans'
+print(len(myname))
